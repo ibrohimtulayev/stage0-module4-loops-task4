95adaf447ac496870daae97ce20b7f9ada8e0871
Ibrohimtulayev <116336786+ibrohimtulayev@users.noreply.github.com>
2023-11-16 16:00:33 +0500

    ,

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (numberToGoUntil < toBreakWith) {
+            System.out.println("iterating till the end");
+            int i = 1;
+            while (i <= numberToGoUntil) {
+                System.out.println(i);
+                i++;
+            }
+        } else {
+            int i = 1;
+            while (i <= toBreakWith) {
+                System.out.println(i);
+                i++;
+            }
 
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int i = 0;
+        while(lastPrinted>i){
+            for (int j = i; j <=lastPrinted ; j++) {
+                if (i%3!=0) System.out.println(j);
+                i++;
+            }
+        }
     }
 }
